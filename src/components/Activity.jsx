c1ea792459482073f16e2922eee2a6a7b74cf7c8
Sean R Reid <sean@torchcodelab.com>
2020-04-07 09:51:43 -0400

    Updated Activity component with actions and props from state

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { eat } from "../redux/actions";
 
-const Activity = props => (
-    <h1>Activity Component</h1>
+const Activity = ({ activity, eat }) => (
+    <>
+        <h1>What is the cat doing?</h1>
+        <p>The cat is {activity}</p>
+        <button onClick={()=> eat()}>Eating</button>
+    </>
 )
 
-export default Activity;
+const mapStateToProps = state => {
+    const { activity } = state;
+    return activity;
+}
+
+export default connect(
+    mapStateToProps, // mapStateToProps - What do I want?
+    { eat } // mapDispatchToProps - What do I want to do?
+)(Activity);
